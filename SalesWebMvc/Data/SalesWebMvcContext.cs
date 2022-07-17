ad8b9322eb89a1084a60b4dc0e3ab0aaf0fc43fd
GabrielBarbosaDaConceicao <gabriel.bc.dev@hotmail.com>
2022-07-17 15:42:29 -0300

    Mvc Aplication/Crud

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
